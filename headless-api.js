6edddb288f05d5104c0979a1a821dccd76293a30
nusantarapras <pras.elang@gmail.com>
2025-06-24 15:57:02 +0700

    Update headless-api.js

--- headless-api.js
+++ headless-api.js
@@ -90,3 +90,12 @@ function getValueByFieldName(fields, fieldName) {
     return undefined;
 }
 
+function formatISO(date){
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() dimulai dari 0
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+}
+
+
